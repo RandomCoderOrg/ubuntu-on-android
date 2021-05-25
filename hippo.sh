9a3df55f8dfd65c243f5fb50fbef200f3e6e217a
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2021-05-25 18:37:35 +0530

    added script file
    initialization

---
+++ hippo.sh
@@ -0,0 +1,49 @@
+##
+## Plug-in for installing Hippo.
+##
+
+DISTRO_NAME="hippo"
+DISTRO_COMMENT="[Testing] A ubuntu 21.04 port with some tweaks"
+
+# You can override a CPU architecture to let distribution
+# be executed by QEMU (user-mode).
+#
+# You can specify the following values here:
+#
+#  * aarch64: AArch64 (ARM64, 64bit ARM)
+#  * armv7l:  ARM (32bit)
+#  * i686:    x86 (32bit)
+#  * x86_64:  x86 (64bit)
+#
+# Default value is set by proot-distro script and is equal
+# to the CPU architecture of your device (uname -m).
+#DISTRO_ARCH=$(uname -m)
+
+# Returns download URL and SHA-256 of file in this format:
+# SHA-256|FILE-NAME
+get_download_url() {
+	local rootfs
+	local sha256
+
+	case "$DISTRO_ARCH" in
+		aarch64)
+			rootfs=""
+			sha256="a35d4fb115da45f866d0b41544f99587eba979dad584bf9c9982f6242cc002ed"
+			;;
+	esac
+
+	echo "${sha256}|${rootfs}"
+}
+
+
+distro_setup() {
+	# Hint: $PWD is the distribution rootfs directory.
+	#echo "hello world" > ./etc/motd
+
+	# Run command within proot'ed environment with
+	# run_proot_cmd function.
+	# Uncomment this to do system upgrade during installation.
+	#run_proot_cmd apt update
+	#run_proot_cmd apt upgrade -yq
+	:
+}
