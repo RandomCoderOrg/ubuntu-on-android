13ee270f15fea79536fa6b5b88d4765beb9a5ac1
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2021-05-30 18:45:19 +0530

    Preaparing for first xfce4 fs file pre-release 	modified:   README.md 	new file:   etc/scripts/dbus-wake.sh 	new file:   etc/scripts/hippo 	new file:   gitignore 	modified:   install.sh

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # ubuntu-on-android
-![status](https://img.shields.io/badge/status-in%20early%20stages%20of%20buliding-orange)
-
 A twaked ubuntu-21.04 port runs on android with termux/proot-distro
 
 ## More updates comming soon👊.
--- install.sh
+++ install.sh
@@ -4,20 +4,30 @@
 # A Script to implant hippo inside 
 # proot-distro(for now!)
 #
-
-DISTRO_PLUGINS_DIR="/data/data/com.termux/files/usr/etc/proot-distro"
+TERMUX_PREFIX="/data/data/com.termux/files"
+DISTRO_PLUGINS_DIR="${TERMUX_PREFIX}/usr/etc/proot-distro"
 
 function _implant_()
 {
-    if [ -r hippo.sh ]; then
-        mv -v hippo.sh "${DISTRO_PLUGINS_DIR}"
+    if [ -f hippo.sh ]; then
+        if ! [ -f "${DISTRO_PLUGINS_DIR}"/hippo.sh ]; then
+            mv -v hippo.sh "${DISTRO_PLUGINS_DIR}"
+        else
+            echo "Looks like \"hippo\" is already installed..."
+        fi
         return 0
     else
         return 1
     fi
 }
 
+if ! command -v proot-distro; then
+    apt install proot-distro -y
+fi
+
 if _implant_; then
     echo -e "Implant done......."
-    proot-distro list | grep "hippo"
+    echo -e "- Now you can install ubuntu by running \e[1;32mproot-distro install\e[0m"
+else # this wont happen (mostly)
+    echo ":( \e[32m error...\e[0m Please create a issue at \e[1;32mhttps://github.com/SaicharanKandukuri/ubuntu-on-android/issues\e[0m to resolve "
 fi
