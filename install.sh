4ecef3fa17e51e15fdc2e867640861cde6091395
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2021-05-30 16:23:54 +0530

    hippo-raw
    modified scripts to for raw ubuntu fs
    modified installer script

--- install.sh
+++ install.sh
@@ -9,8 +9,12 @@ DISTRO_PLUGINS_DIR="/data/data/com.termux/files/usr/etc/proot-distro"
 
 function _implant_()
 {
-    if [ -r hippo.sh ]; then
-        mv -v hippo.sh "${DISTRO_PLUGINS_DIR}"
+    if [ -r hippo-raw.sh ]; then
+        if ! [ -r "${DISTRO_PLUGINS_DIR}"/hippo-raw.sh ]; then
+            mv -v hippo.sh "${DISTRO_PLUGINS_DIR}"
+        else
+            echo "Looks like \"hippo-raw\" is already installed..."
+        fi
         return 0
     else
         return 1
@@ -19,5 +23,7 @@ function _implant_()
 
 if _implant_; then
     echo -e "Implant done......."
-    proot-distro list | grep "hippo"
+    echo -e "- Now you can install ubuntu by running \e[1;32mproot-distro install\e[0m"
+else
+    echo ":( \e[32m error...\e[0m Please create a issue at \e[1;32mhttps://github.com/SaicharanKandukuri/ubuntu-on-android/issues\e[0m to resolve "
 fi
