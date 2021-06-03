108f142b7357b3f24bdeae96315dcdd1f9f44ad0
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2021-06-03 13:38:26 +0530

    modified:   install.sh 	added pulseaudio to requirement 	and some minor code

--- install.sh
+++ install.sh
@@ -12,22 +12,35 @@ function _implant_()
     if [ -f hippo.sh ]; then
         if ! [ -f "${DISTRO_PLUGINS_DIR}"/hippo.sh ]; then
             mv -v hippo.sh "${DISTRO_PLUGINS_DIR}"
+            echo -e "Implant done......."
+            echo -e "- Now you can install ubuntu by running \e[1;32mproot-distro install hippo\e[0m"
         else
             echo "Looks like \"hippo\" is already installed..."
+            echo -e "you can install ubuntu by running \e[1;32mproot-distro install hippo\e[0m"
+            echo -e "you can login to ubuntu by running \e[1;32mproot-distro login hippo\e[0m" 
+        
         fi
         return 0
     else
         return 1
     fi
 }
+############################
+#
+# REQUIREMENTS
+
+apt update; apt upgrade -y
 
 if ! command -v proot-distro; then
     apt install proot-distro -y
 fi
+if ! command -v pulseaudio; then
+    apt install pulseaudio -y
+fi
 
+########################
 if _implant_; then
-    echo -e "Implant done......."
-    echo -e "- Now you can install ubuntu by running \e[1;32mproot-distro install hippo \e[0m"
+    return 0
 else # this wont happen (mostly)
     echo ":( \e[32m error...\e[0m Please create a issue at \e[1;32mhttps://github.com/SaicharanKandukuri/ubuntu-on-android/issues\e[0m to resolve "
 fi
