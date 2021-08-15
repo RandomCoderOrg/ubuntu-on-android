38db4513f17c2a321f6d8c5097798db1d3421dae
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-08-15 18:29:48 +0530

    adapted to new changes in `proot-distro` - `v2`
    new changes in proot-distro caused to strip the filesystem during extraction
    but it fixed now

--- hippo.sh
+++ hippo.sh
@@ -1,6 +1,8 @@
 DISTRO_NAME="hippo"
 DISTRO_COMMENT="A ubuntu 21.04 port with some tweaks xfce4"
 
+TARBALL_STRIP_OPT=0
+
 TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v02-xfce4/hippo-arm64-v02-xfce4.tar.gz"
 TARBALL_SHA256['aarch64']="282a7498046ef0e4f0a57265725d739e67048bb2b64ba2f4441ca4ce1a67ea40"
 TARBALL_URL['arm']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v02-xfce4/hippo-armhf-v02-xfce4.tar.gz"
