54fb652a0e61f3f424c63a12ce565daf07ede219
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-09-23 08:41:18 +0530

    make archive compatible for proot-distro `v2`

--- hippo.sh
+++ hippo.sh
@@ -5,45 +5,7 @@
 DISTRO_NAME="hippo"
 DISTRO_COMMENT="[Testing] A ubuntu 21.04 port with some tweaks"
 
-# You can override a CPU architecture to let distribution
-# be executed by QEMU (user-mode).
-#
-# You can specify the following values here:
-#
-#  * aarch64: AArch64 (ARM64, 64bit ARM)
-#  * armv7l:  ARM (32bit)
-#  * i686:    x86 (32bit)
-#  * x86_64:  x86 (64bit)
-#
-# Default value is set by proot-distro script and is equal
-# to the CPU architecture of your device (uname -m).
-#DISTRO_ARCH=$(uname -m)
+TARBALL_STRIP_OPT=0
 
-# Returns download URL and SHA-256 of file in this format:
-# SHA-256|FILE-NAME
-get_download_url() {
-	local rootfs
-	local sha256
-
-	case "$DISTRO_ARCH" in
-		aarch64)
-			rootfs="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v01-xfce4/ubuntu-21.04-xfce4-rootfs-v01-arm64.tar.gz"
-			sha256="e3bbb6f88cff4839fe6a607929708aa84b7fb2e1dc58a2d101471c4150c4447f"
-			;;
-	esac
-
-	echo "${sha256}|${rootfs}"
-}
-
-
-distro_setup() {
-	# Hint: $PWD is the distribution rootfs directory.
-	#echo "hello world" > ./etc/motd
-
-	# Run command within proot'ed environment with
-	# run_proot_cmd function.
-	# Uncomment this to do system upgrade during installation.
-	#run_proot_cmd apt update
-	#run_proot_cmd apt upgrade -yq
-	:
-}
+TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v01-xfce4/ubuntu-21.04-xfce4-rootfs-v01-arm64.tar.gz"
+TARBALL_SHA256['aarch64']="e3bbb6f88cff4839fe6a607929708aa84b7fb2e1dc58a2d101471c4150c4447f"
