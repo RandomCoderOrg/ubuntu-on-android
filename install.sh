1e1183fa8f073639fc7afd62f999e35d615f50ce
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-10-17 12:49:55 +0530

    make script clean

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 ####################################################
 # UDROID installer Script V01 (mad installer)
 # A script made by @zman-1x1 saicharankandukuri
-# Copyright (c) 2021 Saicharan Kandukuri
-# hello.1x1.sai@gmail.com
+# Copyright (c) 2021 Saicharan Kandukuri <hello.1x1.sai@gmail.com>
 
 
 version="01"
@@ -13,7 +12,7 @@ installer_authors="saicharankandukuri"
 
 # * Deafault color is Blue
 RST="\e[0m"
-RED="\e[1;31m" # *This is bold 
+RED="\e[1;31m" # *This is bold
 GREEN="\e[1;32m"
 BLUE="\e[34m"
 DC=${BLUE}
@@ -27,13 +26,13 @@ if [ -n "$HIPPO_BRANCH" ]; then
 fi
 
 
-# CACHE_ROOT is the place where are download caches are stored 
+# CACHE_ROOT is the place where are download caches are stored
 CACHE_ROOT="${HOME}/.uoa-cache-root"
 
 # TPREFIX where root of termux starts
 TPREFIX="/data/data/com.termux/files"
 
-# BIN_DIR where binary files are stored in termux 
+# BIN_DIR where binary files are stored in termux
 # * (like /bin in traditional linux)
 BIN_DIR="${TPREFIX}/usr/bin"
 
@@ -70,26 +69,28 @@ DEPENDS="proot-distro pulseaudio git"
 # shout()   pring messege in a good way with some lines
 # lshout()  print messege in a standard way
 
-die    () { echo -e "${RED}Error ${*}${RST}";exit 1 ;:;}
-warn   () { echo -e "${RED}Error ${*}${RST}";:;}
+die    () { echo -e "${RED}!!: ${*}${RST}";exit 1 ;:;}
+warn   () { echo -e "${RED}??: ${*}${RST}";:;}
 shout  () { echo -e "${DC}-----";echo -e "${*}";echo -e "-----${RST}";:; }
-lshout () { echo -e "${DC}";echo -e "${*}";echo -e "${RST}";:; }
+lshout () { echo -e "${DC}${*}${RST}";:; }
 
 
 shout "\e[1;32m Udroid Installer v${version} ${version_code_name} \n by ${installer_authors}"
 sleep 2
 
 case $(uname -m) in
-    aarch64|armv7l|armv8l) : ;;
-    *) 
-    die ": sorry Only aarch64 armv7l armv8l os architecture is supported for now"
+    aarch64|armv7l) : ;;
+    armv8l) warn "armv8l may cause issues";;
+    *)
+    die ": sorry Only aarch64 armv7l os architecture is supported for now"
     ;;
 esac
+
 ######################################
 # * function setup_and_clone
 #
 # 1. install required programs
-# 2. remove previous cache if found 
+# 2. remove previous cache if found
 # 3. clone code from links in HIPPO_REPO_URL & FSM_URL
 # 4. call install function
 # if anything goes wrong or any program in code fails kill the installation by calling die function
@@ -127,14 +128,13 @@ function setup_and_clone()
 
     shout "Cloning code from Github........."
     if [ -n "${BRANCH}" ]; then
-        git clone -b "${BRANCH}" ${HIPPO_REPO_URL} "${CACHE_ROOT}/ubuntu-on-android" || die "failed to clone repo"
-        git clone -b "${BRANCH}" ${FSM_URL} "${CACHE_ROOT}/fs-manager-udroid" || die "failed to clone repo"
+        git clone -b "${BRANCH}" ${HIPPO_REPO_URL} "${CACHE_ROOT}/ubuntu-on-android" || die "failed to clone repo ubuntu-on-android.."
+        git clone -b "${BRANCH}" ${FSM_URL} "${CACHE_ROOT}/fs-manager-udroid" || die "failed to clone repo fs-manager-udroid"
     else
-        git clone ${HIPPO_REPO_URL} "${CACHE_ROOT}/ubuntu-on-android" || die "failed to clone repo"
-        git clone ${FSM_URL} "${CACHE_ROOT}/fs-manager-udroid" || die "failed to clone repo"
+        git clone ${HIPPO_REPO_URL} "${CACHE_ROOT}/ubuntu-on-android" || die "failed to clone repo ubuntu-on-android \"${BRANCH}\""
+        git clone ${FSM_URL} "${CACHE_ROOT}/fs-manager-udroid" || die "failed to clone repo fs-manager-udroid \"${BRANCH}\""
     fi
     lshout "Done..."
-
     install
 }
 
@@ -152,7 +152,7 @@ function install()
     shout "setting up proot-distro udroid implant..."
 
     sleep 3
-    
+
     if [ -f ${SCRIPT_DIR} ]; then
         mv ${SCRIPT_DIR} "${SCRIPT_DIR}1"
     fi
@@ -168,12 +168,11 @@ function install()
         cd "${oldpwd}" || die "error"
     fi
 
-    
-     shout "setup complete...\nNow you can install and login with comand ${GREEN}udroid${DC} \nfor info use udroid --help"
-     lshout "for additional documentation see: https://github.com/RandomCoderOrg/ubuntu-on-android#basic-usage"
-     lshout "report issues and feature requests at: https://github.com/RandomCoderOrg/ubuntu-on-android/issues"
-     lshout "Join Our discord server: https://discord.gg/u7KDGUc3Aw"
-    exit 1
+
+    shout "setup complete...\nNow you can install and login with comand ${GREEN}udroid${DC} \nfor info use udroid --help"
+    lshout "for additional documentation see: https://github.com/RandomCoderOrg/ubuntu-on-android#basic-usage"
+    lshout "report issues and feature requests at: https://github.com/RandomCoderOrg/ubuntu-on-android/issues"
+    lshout "Join Our discord server: https://discord.gg/u7KDGUc3Aw"
 
 }
 
