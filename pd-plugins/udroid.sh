7e954fccfa81e3324aa09978fae926dbd3254a4b
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-12-24 11:21:35 +0530

    add new templates

--- pd-plugins/udroid.sh
+++
@@ -1,9 +0,0 @@
-DISTRO_NAME="udroid"
-DISTRO_COMMENT="A ubuntu 21.04 port with some tweaks xfce4"
-
-TARBALL_STRIP_OPT=0
-
-TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v02-xfce4/hippo-arm64-v02-xfce4.tar.gz"
-TARBALL_SHA256['aarch64']="282a7498046ef0e4f0a57265725d739e67048bb2b64ba2f4441ca4ce1a67ea40"
-TARBALL_URL['arm']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v02-xfce4/hippo-armhf-v02-xfce4.tar.gz"
-TARBALL_SHA256['arm']="e797c8590644ffd968dc1727474b533f7bc32e22f69a5f91e519614315baf211"
