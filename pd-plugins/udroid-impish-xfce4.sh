bc28b7ec9b1ad4829879533141d878b66671f276
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-12-26 17:11:22 +0530

    test

--- pd-plugins/udroid-impish-xfce4.sh
+++ pd-plugins/udroid-impish-xfce4.sh
@@ -5,9 +5,6 @@ DISTRO_COMMENT="A ubuntu 21.04 port with some tweaks xfce4"
 
 TARBALL_STRIP_OPT=0
 
-TARBALL_URL['aarch64']=""
-TARBALL_SHA256['aarch64']=""
-TARBALL_URL['amd64']=""
-TARBALL_SHA256['amd64']=""
-TARBALL_URL['arm']=""
-TARBALL_SHA256['arm']=""
+TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v3/udroid-impish-xfce4-beta.tar.gz"
+TARBALL_SHA256['aarch64']="dc5a75a4cf6b8948851d192174a265caf7639ac4c9c42aa436bebbf38873cfa8"
+
