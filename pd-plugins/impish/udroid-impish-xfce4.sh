058c60489cc3817338cb00664497f8291d1cd6c9
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2022-04-17 23:29:23 +0530

    upgrade udroid v3 xfce4 beta - > `V3MBB2`

--- pd-plugins/impish/udroid-impish-xfce4.sh
+++ pd-plugins/impish/udroid-impish-xfce4.sh
@@ -5,7 +5,7 @@ DISTRO_COMMENT="A ubuntu 21.10 port with some tweaks xfce4"
 
 TARBALL_STRIP_OPT=0
 
-TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v3/udroid-impish-xfce4-beta.tar.gz"
-TARBALL_SHA256['aarch64']="dc5a75a4cf6b8948851d192174a265caf7639ac4c9c42aa436bebbf38873cfa8"
+TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v3/udroid-arm64-xfce4-V3MBB2.tar.gz"
+TARBALL_SHA256['aarch64']="617dc4b18bd27b181ebabeadf8964f23b6e8087da75c253ead88dba1e7931f1d"
 TARBALL_URL['x86_64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v3/udroid-xfce4-impish-v3-build01.tar.gz"
 TARBALL_SHA256['x86_64']="88a5f5f079aef994b6d5b2e3df773aa875ed2157d1f00c8fda504bda9f01e680"
