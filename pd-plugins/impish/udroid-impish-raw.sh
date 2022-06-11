7d853051e2e1a6f82e2beb9d8aa35b92a4877f1d
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2022-06-11 09:42:45 +0000

    Add new tarball links https://github.com/RandomCoderOrg/udroid-download/releases/tag/V3R3

--- pd-plugins/impish/udroid-impish-raw.sh
+++ pd-plugins/impish/udroid-impish-raw.sh
@@ -5,9 +5,9 @@ DISTRO_COMMENT="A ubuntu 21.10 port with some tweaks xfce4"
 
 TARBALL_STRIP_OPT=0
 
-TARBALL_URL['aarch64']=""
-TARBALL_SHA256['aarch64']=""
-TARBALL_URL['amd64']=""
-TARBALL_SHA256['amd64']=""
-TARBALL_URL['arm']=""
-TARBALL_SHA256['arm']=""
+TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/udroid-download/releases/download/V3R3/impish-raw-arm64.tar.gz"
+TARBALL_SHA256['aarch64']="12c1a5245d95cbd1fc6fec3e5f387ddfcf9b7799e4608187d60c0ad30700ff13"
+TARBALL_URL['amd64']="https://github.com/RandomCoderOrg/udroid-download/releases/download/V3R3/impish-raw-amd64.tar.gz"
+TARBALL_SHA256['amd64']="c5ce02ed7be92537bb1e2bcc5eb2ab588afc50c147972e1336a2a9c290813245"
+TARBALL_URL['arm']="https://github.com/RandomCoderOrg/udroid-download/releases/download/V3R3/impish-raw-armhf.tar.gz"
+TARBALL_SHA256['arm']="71901267978453931195002e04f1cc27e0d1aaff6eecda36675aa38c33938eef"
