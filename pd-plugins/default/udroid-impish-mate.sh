8654989d87f72a2d64427393f0e606102567e634
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2022-07-24 05:26:55 +0000

    set impish to default

---
+++ pd-plugins/default/udroid-impish-mate.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DISTRO_NAME="udroid"
+DISTRO_COMMENT="A ubuntu 21.10 port with some tweaks xfce4"
+
+TARBALL_STRIP_OPT=0
+
+TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v3/udroid-mate-arm64-betav2.tar.gz"
+TARBALL_SHA256['aarch64']="3069a4c40c6d2982e943216f46ce52516a3c8d5ae29bdde633237ddd3748c88b"
+# TARBALL_URL['amd64']=""
+# TARBALL_SHA256['amd64']=""
+# TARBALL_URL['arm']=""
+# TARBALL_SHA256['arm']=""
